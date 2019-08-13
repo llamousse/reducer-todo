f308cbdf83053ed5259a06858568bd76f633332c
Vicky <vyue013@gmail.com>
2019-08-13 13:10:25 -0700

    Built simple reducer and initial state

---
+++ reducer-todo/src/reducers/todoReducer.js
@@ -0,0 +1,23 @@
+export const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+};
+  
+export const todoReducer = (state, action) => {
+    switch (action.type) {
+    //   case 'TOGGLE_EDITING':
+    //     return {
+    //       ...state,
+    //       editing: !state.editing
+    //     };
+    //   case 'UPDATE_TITLE':
+    //     return {
+    //       ...state,
+    //       title: action.payload,
+    //       editing: !state.editing
+    //     };
+        default:
+        return state;
+    }
+};
